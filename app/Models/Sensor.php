2627fc5c6572e74f99d090be7debc356518651a4
rahirahii <rahiiany.viclarry@gmail.com>
2025-08-12 15:45:01 -0300

    criação da model e migration Sensor

---
+++ app/Models/Sensor.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sensor extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'ambiente_id',
+        'codigo',
+        'tipo',
+        'descricao',
+        'status'
+    ];
+
+    public function registro()
+    {
+        return $this->hasMany(Registro::class);
+    }
+
+    public function ambiente()
+    {
+        return $this->belongsTo(Ambiente::class);
+    }
+}
