e60ff133f8c0f077de61309bf818f698ccbb255b
rahirahii <rahiiany.viclarry@gmail.com>
2025-08-14 15:51:29 -0300

    criação de rotas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Livewire\AmbienteCreate;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//crud- ambiente
+Route::get('/ambiente/create', AmbienteCreate::class);
