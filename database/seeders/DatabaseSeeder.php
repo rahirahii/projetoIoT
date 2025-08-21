0b7f4c69cc30ff610f930be41c79ac05c268f01d
rahirahii <rahiiany.viclarry@gmail.com>
2025-08-21 14:06:29 -0300

    Alterações nas seeders (registro e database)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Ambiente;
+use App\Models\Registro;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,6 +21,10 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             SensorSeeder::class
+        ]);
+
+        $this->call([
+            RegistroSeeder::class
         ]);
     }
 }
