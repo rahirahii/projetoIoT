fc2580d5c50333b102d51590cf2f95e4b418009e
rahirahii <rahiiany.viclarry@gmail.com>
2025-08-26 12:57:56 -0300

    criação do Edit (Sensor)

---
+++ app/Livewire/SensorEdit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class SensorEdit extends Component
+{
+    public function render()
+    {
+        return view('livewire.sensor-edit');
+    }
+}
