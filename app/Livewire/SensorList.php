0342b00f7f9f0a209eabb64746b898b44ed4ef27
rahirahii <rahiiany.viclarry@gmail.com>
2025-08-26 15:39:27 -0300

    alterações no sensor list

--- app/Livewire/SensorList.php
+++ app/Livewire/SensorList.php
@@ -2,12 +2,28 @@
 
 namespace App\Livewire;
 
+use App\Models\Sensor;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class SensorList extends Component
 {
+    use WithPagination;
+    public $search = "";
+    public $perPage = 15;
+
+    protected $queryString = [
+        'search' => ['except' => ''],
+        'perPage' => ['except' => '15']
+
+    ];
     public function render()
     {
-        return view('livewire.sensor-list');
+        $sensores = Sensor::where('tipo', 'like', "%{$this->search}%")
+            ->orWhere('codigo', 'like', "%{$this->search}%")
+            ->paginate($this->perPage);
+
+        $sensores = Sensor::where('codigo', 'like', "%{$this->search}%");
+        return view('livewire.sensor-list', compact('sensores'));
     }
 }
