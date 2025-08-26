fa3a3660f51f936f83bbb777efc1f909feb9981b
rahirahii <rahiiany.viclarry@gmail.com>
2025-08-26 14:20:40 -0300

    criação de rotas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Livewire\Dashboard;
+use App\Livewire\SensorCreate;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', Dashboard::class);
+
+//CRUD-sensores
+Route::get('/sensor/create', SensorCreate::class);
