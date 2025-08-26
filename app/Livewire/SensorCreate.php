ea0f2fb2dc44a15077449883350eb8fe84a51d33
rahirahii <rahiiany.viclarry@gmail.com>
2025-08-26 12:57:31 -0300

    criação do create e do blade(Sensor)

---
+++ app/Livewire/SensorCreate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class SensorCreate extends Component
+{
+    public function render()
+    {
+        return view('livewire.sensor-create');
+    }
+}
