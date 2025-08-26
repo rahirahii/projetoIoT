588f3c80097387cc0746c1f6095998d8c61e4687
rahirahii <rahiiany.viclarry@gmail.com>
2025-08-26 12:58:10 -0300

    criação do List (Sensor)

---
+++ app/Livewire/SensorList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class SensorList extends Component
+{
+    public function render()
+    {
+        return view('livewire.sensor-list');
+    }
+}
