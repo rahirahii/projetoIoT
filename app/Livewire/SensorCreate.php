1c03e740a1b122d2749916a2179b4a6444daa651
rahirahii <rahiiany.viclarry@gmail.com>
2025-08-26 14:20:30 -0300

    alterações no sensor create e sensor create.blade

--- app/Livewire/SensorCreate.php
+++ app/Livewire/SensorCreate.php
@@ -2,10 +2,54 @@
 
 namespace App\Livewire;
 
+use App\Models\Ambiente;
+use App\Models\Sensor;
 use Livewire\Component;
 
 class SensorCreate extends Component
 {
+    public $ambiente_id;
+    public $codigo;
+    public $tipo;
+    public $descricao;
+    public $status;
+
+    protected $rules = [
+        'codigo' => 'required|unique:sensors,codigo',
+        'tipo' => 'required|min:3|max:10',
+        'descricao' => 'required|min:6|max:20'
+    ];
+
+    protected $messages = [
+        'codigo.required' => 'o código é obrigatório',
+        'codigo.unique' => 'o campo codigo é unico',
+        'tipo.required' => 'o campo tipo é obrigatório',
+        'tipo.max' => 'o número máximo de caracteres é 10',
+        'tipo.min' => 'o número minimo de caracteres é 3',
+        'descricao.required' => 'a descrição é obrigatória',
+        'descricao.min' => 'o numero minímo de caracteres é 6',
+        'descricao.max' => 'o numero máximo de caracteres é 20'
+    ];
+
+    public function store()
+    {
+        $this->validate();
+        $ambiente = Ambiente::create([
+            'ambiente_id' => $this->ambiente_id
+        ]);
+
+        Sensor::create([
+            'codigo' => $this->nome,
+            'tipo' => $this->tipo,
+            'descricao' => $this->tipo,
+            'status' => $this->status,
+            'ambiente_id' => $ambiente->id
+        ]);
+
+        session()->flash('success  ', 'Cadastro de Sensor realizado com sucesso');
+        return redirect()->route('sensor.list');
+    }
+
     public function render()
     {
         return view('livewire.sensor-create');
--- resources/views/livewire/sensor-create.blade.php
+++ resources/views/livewire/sensor-create.blade.php
@@ -1,3 +1,49 @@
-<div>
-    {{-- The best athlete wants his opponent at his best. --}}
+<div class="container">
+    <div class="mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <div class="card shadow-sm">
+                    <div class="card-header">
+                        <h4 style="text-align:center">Cadastro de Sensor</h4>
+                    </div>
+
+                    <div class="card-body">
+                        <form wire:submit.prevent="store">
+                            <div class="mb-3">
+                                <label for="codigo" class="form-label">Codigo do sensor</label>
+                                <input type="text" class="form-control" id="sensor" name="sensor"
+                                    placeholder="Código do sensor" wire:model.defer="sensor"
+                                    style="border-radius: 100px; border-inline-color: black; border-block-color:black">
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="tipo" class="form-label">Tipo</label>
+                                <input type="text" class="form-control" id="tipo" name="tipo"
+                                    placeholder="Tipo" wire:model.defer="tipo"
+                                    style="border-radius: 100px; border-inline-color: black; border-block-color:black">
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="descricao" class="form-label">Descrição</label>
+                                <input type="text" class="form-control" id="descricao" name= "descricao" placeholder="Descrição"
+                                    wire:model.defer="descricao"
+                                    style="border-radius: 100px; border-inline-color: black; border-block-color:black">
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="status" class="form-label">Status</label>
+                                <select class="form-select" id="status" name= "status" 
+                                    wire:model.defer="status"
+                                    style="border-radius: 100px; border-inline-color: black; border-block-color:black">
+                            </div>
+
+                            <div class="mb-3">
+                                <button type="submit" class="btn btn-primary">Cadastrar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
