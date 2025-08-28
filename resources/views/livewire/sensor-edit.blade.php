bdeb3503f980639d617533a7c1714142b1542e25
rahirahii <rahiiany.viclarry@gmail.com>
2025-08-28 14:22:19 -0300

    alterações no edit e  no .blade (sensor)

--- resources/views/livewire/sensor-edit.blade.php
+++ resources/views/livewire/sensor-edit.blade.php
@@ -1,44 +1,54 @@
-<div class="row mb-3">
-    <div class="col-md-6">
-        <input type="text" wire:model.lazy="search" class="form-control" placeholder="Buscar Sensor...">
+<div class="container mt-5">
+    <div class="bg-transparent border-0 d-flex justify-content-between align-items-center mb-4 pt-3">
+        <h2><i class="bi bi-person-circle"></i> Editar Sensor</h2>
     </div>
-    <div class="col-md-3">
-        <select wire:model.live="perPage" class="form-select">
-            <option value="10">10 por pagina</option>
-            <option value="25">25 por pagina</option>
-            <option value="50">50 por pagina</option>
-            <option value="100">100 por pagina</option>
-        </select>
-    </div>
-</div>
 
-<div class="card">
-    <div class="shadow rounded-4">
-        <div class="card-header d-flex justify-content-between align-items-center">
-            <h5 class="mb-0">Sensores</h5>
-            <a class="btn btn-sm" href="{{ route('sensor.create') }}">
-                <i class="bi bi-plus-circle"></i> Novo Sensor
-            </a>
-        </div>
-        <div class="card-body p-0">
-            <table class="table table-hover">
-                <thead class="table-light">
-                    <th>ID</th>
-                    <th>Código</th>
-                    <th>Tipo</th>
-                    <th>descrição</th>
-                    <th>Status</th>
-                </thead>
-                <tbody>
-                    @foreach ($sensor as $s)
-                        <tr>
-                            <td>{{ $s->codigo }}</td>
-                            <td>{{ $s->tipo }}</td>
-                            <td>{{ $s->descricao }}</td>
-                            <td>{{ $s->status }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
+    <div class="card">
+        <div class="card-body">
+            @if (session()->has('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
+            @endif
+
+            <form wire:submit.prevent="update">
+                <div class="row">
+                    <div class="col-md-6 mb-3">
+                        <label for="codigo" class="form-label">Código</label>
+                        <input type="text" wire:model="codigo" id="codigo" class="form-control">
+                        @error('codigo')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="tipo" class="form-label">Tipo</label>
+                        <input type="text" wire:model="tipo" id="tipo" class="form-control">
+                        @error('tipo')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                     <div class="col-md-6 mb-3">
+                        <label for="descricao" class="form-label">Descrição</label>
+                        <input type="text" wire:model="descricao" id="descricao" class="form-control">
+                        @error('descricao')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="status" class="form-label">Status</label>
+                        <select class="form-select" id="status" name= "status" wire:model.defer="status"
+                            style="border-radius: 100px; border-inline-color: black; border-block-color:black">
+                            <option hidden>Selecione o status do sensor</option>
+                            <option value="1">Ativo</option>
+                            <option value="0">Desativado</option>
+                            <select>
+                    </div>
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                        <button type="submit" class="btn btn-primary">
+                            <i class="bi bi-save"></i> Salvar Alterações
+                        </button>
+                    </div>
+            </form>
         </div>
     </div>
 </div>
