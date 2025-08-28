a38e8df2e803b01e725f950f1eb875d457c7d39c
rahirahii <rahiiany.viclarry@gmail.com>
2025-08-28 07:49:43 -0300

    criaçãoda função list

--- app/Livewire/AmbienteList.php
+++ app/Livewire/AmbienteList.php
@@ -3,9 +3,16 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class AmbienteList extends Component
 {
+    use WithPagination;
+
+    public $search="";
+    public $perPage="";
+
+
     public function render()
     {
         return view('livewire.ambiente-list');
