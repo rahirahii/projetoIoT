ef8701e130939bc4ee73896bebd520f5ae6c0009
rahirahii <rahiiany.viclarry@gmail.com>
2025-08-28 10:13:01 -0300

    criação e alteração das rotas do crud de ambiente

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Livewire\AmbienteCreate;
+use App\Livewire\AmbienteEdit;
+use App\Livewire\AmbienteList;
 use Illuminate\Support\Facades\Route;
 
 //CRUD - ambiente
 Route::get('/ambiente/create', AmbienteCreate::class);
+Route::get('/ambiente/list', AmbienteList::class);
+Route::get('/ambiente/edit/{id}', AmbienteEdit::class);
