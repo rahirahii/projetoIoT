c58ca7fbf8a77b7765c0c1569f218aaebcbe2709
rahirahii <rahiiany.viclarry@gmail.com>
2025-08-28 14:23:28 -0300

    alterações na rota

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Livewire\Dashboard;
 use App\Livewire\SensorCreate;
+use App\Livewire\SensorEdit;
 use App\Livewire\SensorList;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +12,4 @@ Route::get('/', Dashboard::class);
 //CRUD-sensores
 Route::get('/sensor/create', SensorCreate::class);
 Route::get('/sensor/list', SensorList::class);
+Route::get('/sensor/{id}/edit/', SensorEdit::class);
