01e8beda76108db7efe1c775239ef6c60fad4e89
rahirahii <rahiiany.viclarry@gmail.com>
2025-08-28 10:12:41 -0300

    Alterações no list e no list.blade (ambiente)

--- app/Livewire/AmbienteList.php
+++ app/Livewire/AmbienteList.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Ambiente;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -9,12 +10,20 @@ class AmbienteList extends Component
 {
     use WithPagination;
 
-    public $search="";
-    public $perPage="";
+    public $search = "";
+    public $perPage = "5";
 
+    protected $queryString = [
+        'search' => ['except' => ''],
+        'perPage' => ['except' => '10']
+    ];
 
     public function render()
     {
-        return view('livewire.ambiente-list');
+        $ambientes = Ambiente::where('nome', 'like', "%{$this->search}%")
+            ->orWhere('descricao', 'like', "%{$this->search}%")
+            ->paginate($this->perPage);
+
+        return view('livewire.ambiente-list', compact('ambientes'));
     }
 }
