e2aa21d63cd9ea3258c9b1e03e8cf94f3a54d71f
rahirahii <rahiiany.viclarry@gmail.com>
2025-08-28 10:11:54 -0300

    Alterações no ambiente edit e no edit.blade (ambiente)

--- app/Livewire/AmbienteEdit.php
+++ app/Livewire/AmbienteEdit.php
@@ -7,22 +7,38 @@ use Livewire\Component;
 
 class AmbienteEdit extends Component
 {
-    public $nome;
-    public $descricao;
-    public $status;
+  public $id;
+  public $nome;
+  public $descricao;
+  public $status;
 
-    public function update()
-    {
-      Ambiente::update([
-        'nome'=>$this->nome,
-        'descricao'=>$this->descricao,
-        'status'=>$this->status
-      ]);
+  public function mount($id)
+  {
+    $ambientes = Ambiente::findOrFail($id);
+    $this->nome = $ambientes->nome;
+    $this->descricao = $ambientes->descricao;
+    $this->status = $ambientes->status;
+  }
 
-    }
+  public function update()
+  {
+    $this->validate([
+      'nome' => 'required|max:80',
+      'descricao' => 'required|min:6|max:80',
+      'status' => 'required',
+    ]);
+    $ambientes = Ambiente::find($this->id);
+    $ambientes->update([
+      'nome' => $this->nome,
+      'descricao' => $this->descricao,
+      'status' => $this->status
+    ]);
 
-    public function render()
-    {
-        return view('livewire.ambiente-edit');
-    }
+    session()->flash('success', 'Ambiente atualizado com sucesso');
+  }
+
+  public function render()
+  {
+    return view('livewire.ambiente-edit');
+  }
 }
