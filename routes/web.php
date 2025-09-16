b5891b42cbaa458082a53636b8bf69bb302b7f2e
rahirahii <rahiiany.viclarry@gmail.com>
2025-09-16 15:26:31 -0300

    rotas do registro

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Livewire\Dashboard;
+use App\Livewire\RegistroList;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Dashboard::class);
+
+//LIST (Registros)
+Route::get('/registro/list', RegistroList::class);
