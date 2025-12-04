8024e3966b64a328b462f4f1b8bd06836632a4af
rahirahii <rahiiany.viclarry@gmail.com>
2025-12-04 15:30:24 -0300

    ambiente .blade

---
+++ resources/views/livewire/Ambiente/ambiente-create.blade.php
@@ -0,0 +1,52 @@
+<div class="container">
+    <div class="mt-5">
+        @if (session()->has('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ session('success') }}
+                <button type="button" class="btn btn-close" data-bs-dismiss="alert" aria-label="close"></button>
+            </div>
+        @endif
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <div class="card shadow-sm">
+                    <div class="card-header">
+                        <h4 style="text-align:center">Cadastro de Ambiente</h4>
+                    </div>
+
+                    <div class="card-body">
+                        <form wire:submit.prevent="store">
+                            <div class="mb-3">
+                                <label for="nome" class="form-label">Nome do ambiente</label>
+                                <input type="text" class="form-control" id="nome" name="nome"
+                                    placeholder="Nome do ambiente" wire:model.defer="nome"
+                                    style="border-radius: 100px; border-inline-color: black; border-block-color:black">
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="descricao" class="form-label">Descrição</label>
+                                <input type="descricao" class="form-control" id="descricao" name="descricao"
+                                    placeholder="Descrição" wire:model.defer="descricao"
+                                    style="border-radius: 100px; border-inline-color: black; border-block-color:black">
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="status" class="form-label">Status</label>
+                                <select class="form-select" id="status" name= "status" wire:model.defer="status"
+                                    style="border-radius: 100px; border-inline-color: black; border-block-color:black">
+                                    <option hidden>Selecione o status da sala</option>
+
+                                    <option value="1">Ativo</option>
+                                    <option value="0">Desativado</option>
+                                    <select>
+                            </div>
+
+                            <div class="mb-3">
+                                <button type="submit" class="btn btn-primary">Cadastrar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
